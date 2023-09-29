d36437faebbf68897cee193508ccef70df9a3c4a
Lukas Günther <lukas.guenther@tib.eu>
2023-09-29 13:21:33 +0200

    start celery before fastapi

--- Komplano/startup.sh
+++ Komplano/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Running both uvicorn and celery worker
-pipenv run uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --root-path /api &
-pipenv run celery -A app.celery worker -l error -c 1 -Q default
+pipenv run celery -A app.celery worker -l error -c 1 -Q default > /dev/null 2>&1 &
+pipenv run uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --root-path /api
