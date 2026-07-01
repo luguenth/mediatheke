90d4da0cec23812190157f1248d0072579a6330c
Lukas Günther <hallo@lguenther.com>
2026-07-01 13:46:34 +0200

    Optimize recommendation engine memory usage

--- Mediatheke/app/src/services/recommendations.py
+++ Mediatheke/app/src/services/recommendations.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from sklearn.feature_extraction.text import TfidfVectorizer
 from ..mediaitem import crud
 from ...core.db.database import get_new_db_session
-from ..mediaitem.model import MediaItem
+
 from sklearn.metrics.pairwise import linear_kernel
 import nltk
 from nltk.corpus import stopwords
@@ -32,24 +32,21 @@ class RecommendationEngine:
     def _load_data(self):
         db = get_new_db_session()
         print("Getting media items for recommendation engine")
-        self.media_items = crud.get_unlimited_media_items(db)
-        texts = [\
-                #item.topic * 1 + " " + \
-                item.title * 1 + " " + \
-                item.description * 1 \
-            for item in self.media_items]        
+        media_items = crud.get_unlimited_media_items(db)
+        self.item_ids = [item.id for item in media_items]
+        self.item_durations = [item.duration for item in media_items]
+        texts = [
+            item.title * 1 + " " +
+            item.description * 1
+            for item in media_items]
         self.vectorizer = TfidfVectorizer(stop_words=german_stop_words)
         self.X = self.vectorizer.fit_transform(texts)
         print("Finished loading data for recommendation engine")
 
     def get_recommendations(self, video_id, **common_params):
-        idx = None
-        for i, item in enumerate(self.media_items):
-            if item.id == video_id:
-                idx = i
-                break
-
-        if idx is None:
+        try:
+            idx = self.item_ids.index(video_id)
+        except ValueError:
             return []
 
         cosine_similarities = linear_kernel(self.X[idx], self.X).flatten()
@@ -57,21 +54,25 @@ class RecommendationEngine:
         # Sort the videos by similarity score
         related_docs_indices = cosine_similarities.argsort()[::-1]
 
-        # Filter out recommendations with the same duration until you have 10 unique durations
+        # Filter out recommendations with the same duration until we hit the limit
         seen_durations = set()
-        recommendations = []
+        recommended_ids = []
         for index in related_docs_indices:
             # Skip the current video itself
             if index == idx:
                 continue
 
-            if len(recommendations) >= common_params["limit"]:
+            if len(recommended_ids) >= common_params["limit"]:
                 break
-            if self.media_items[index].duration not in seen_durations:
-                recommendations.append(self.media_items[index])
-                seen_durations.add(self.media_items[index].duration)
+            if self.item_durations[index] not in seen_durations:
+                recommended_ids.append(self.item_ids[index])
+                seen_durations.add(self.item_durations[index])
 
-        return recommendations
+        if not recommended_ids:
+            return []
+
+        db = get_new_db_session()
+        return crud.get_all_media_items_by_ids(db, ",".join(map(str, recommended_ids)), limit=common_params["limit"])
     
     def get_recommendations_for_text(self, query_text, **common_params):
         # Convert query text into a vector
@@ -83,16 +84,21 @@ class RecommendationEngine:
         # Sort the videos by similarity score
         related_docs_indices = cosine_similarities.argsort()[::-1]
 
-        # Filter out recommendations with the same duration until you have 10 unique durations
+        # Filter out recommendations with the same duration until we hit the limit
         seen_durations = set()
-        recommendations = []
+        recommended_ids = []
         for index in related_docs_indices:
-            if len(recommendations) >= common_params["limit"]:
+            if len(recommended_ids) >= common_params["limit"]:
                 break
-            if self.media_items[index].duration not in seen_durations:
-                recommendations.append(self.media_items[index])
-                seen_durations.add(self.media_items[index].duration)
+            if self.item_durations[index] not in seen_durations:
+                recommended_ids.append(self.item_ids[index])
+                seen_durations.add(self.item_durations[index])
 
+        if not recommended_ids:
+            return []
+
+        db = get_new_db_session()
+        recommendations = crud.get_all_media_items_by_ids(db, ",".join(map(str, recommended_ids)), limit=common_params["limit"])
         print("Recommendations for query", query_text, ":", recommendations)
         return recommendations
     
